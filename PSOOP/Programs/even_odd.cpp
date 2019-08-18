907b4d328f6c03055c01fe9c1c8582d5e27f504a
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 13:24:49 +0530

    Add

---
+++ PSOOP/Programs/even_odd.cpp
@@ -0,0 +1,34 @@
+//using scope resolution operator
+
+#include<iostream>
+using namespace std;
+class example
+{
+	int number;
+	public:
+	void accept();
+	void evenodd();
+};
+
+void example::accept()
+{
+	cout<<"Enter number: ";
+	cin>>number;
+}
+
+void example::evenodd()
+{
+	if(number%2==0)
+		cout<<number<<" is even.";
+	else
+		cout<<number<<" is odd.";
+}
+
+int main()
+{
+	example x;
+	x.accept();
+	x.evenodd();
+	return 0;
+}
+
