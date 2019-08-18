4db35f3dde5ec6defb2dc10f96fb198e8994c34a
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:17:27 +0530

    add friend_class.cpp

---
+++ PSOOP/Programs/friend_class.cpp
@@ -0,0 +1,40 @@
+/* Write a program to show how the class first has generated its friendship to the class second to access private data 
+of class first through public member function of class second*/
+
+
+#include<iostream>
+using namespace std;
+class first
+{
+	friend class second;
+private:
+	int x;
+public:
+	void accept()
+	{
+		cout << "\nEnter value of x: ";
+		cin >> x;
+	}
+
+};
+class second
+{
+
+public:
+	void display(first F);
+
+};
+void second::display(first F)
+{
+	cout << "\nValue of x is: " << F.x;
+
+}
+
+int main()
+{
+	first f1;
+	second s1;
+	f1.accept();
+	s1.display(f1);
+	return 0;
+}
