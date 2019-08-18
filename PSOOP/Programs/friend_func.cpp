1c272adf7843b1c7f7b77c4f4ec463134c638a21
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:18:09 +0530

    add friend_func.cpp

---
+++ PSOOP/Programs/friend_func.cpp
@@ -0,0 +1,31 @@
+/* Write a program to access the private data of a class through friend function*/
+
+
+#include<iostream>
+using namespace std;
+class student
+{
+private:
+	int rollno;
+public:
+	void display();
+	friend void accept(student s);
+};
+void student::display()
+{
+	cout << "\nEnter roll number: ";
+	cin >> rollno;
+	
+}
+void accept(student s)
+{
+	cout << "\nRoll number is: " << s.rollno;
+
+}
+int main()
+{
+	student stud;
+	stud.display();
+	accept(stud);
+	return 0;
+}
