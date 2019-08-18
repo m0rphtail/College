efcf5b72928be37ed923aa7dc1d804d05657d6f0
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:20:16 +0530

    add operator_overloading.cpp

---
+++ PSOOP/Programs/operator_overloading.cpp
@@ -0,0 +1,42 @@
+/*Write a program to create a class of obj as objA and objB the contents of objA is assigned to
+objB using operator overloading technique*/
+
+#include<iostream>
+using namespace std;
+class assign
+{
+private:
+	int x, y;
+public:
+	void getdata(int a, int b);
+	void display();
+	void operator = (assign v);
+};
+void assign::getdata(int a, int b)
+{
+	x = a;
+	y = b;
+
+}
+void assign::operator=(assign v)
+{
+	x = v.x;
+	y = v.y;
+
+}
+void assign::display()
+{
+	cout << "\nValue of x: " << x;
+	cout << "\nValue of y: " << y;
+
+}
+int main()
+{
+
+	assign p1, p2;
+	p1.getdata(20, 30);
+	p1.display();
+	p1 = p2;
+	p2.display();
+	return 0;
+}
