0510ce624ee44dabc3940001394c18abb714e215
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:18:38 +0530

    add nwe_delete.cpp

---
+++ PSOOP/Programs/new_delete.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class value
+{
+private:
+	int x;
+public:
+	value()
+	{
+		x = 0;
+	}
+	void *operator new(size_t size)
+	{
+		return malloc(size);
+	}
+	void operator delete (void* p)
+	{
+		cout << "\nOverloaded delete operator.";
+		free (p);
+
+	}
+
+
+};
+int main()
+{
+	value* v = new value;
+	delete v;
+	return 0;
+}
