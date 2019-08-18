ee53c01a286beb8b620e27ec25b08c55e0ed567a
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:21:04 +0530

    add ptr_to_obj.cpp

---
+++ PSOOP/Programs/ptr_to_obj.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class store
+{
+private:
+	int code;
+	float price;
+public:
+	void accept();
+	void display();
+
+};
+void store::accept()
+{
+	cout << "\nEnter code & price: ";
+	cin >> code >> price;
+
+}
+void store::display()
+{
+	cout << "\nCode & Price is: " << code << price;
+}
+int main()
+{
+	int i;
+	store* s = new store[3];
+	for (i = 0; i < 3; i++)
+	{
+		s->accept();
+		s->display();
+		s++;
+	}
+	return 0;
+}
