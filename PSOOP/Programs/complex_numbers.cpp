5f5bd9b0fddc93d998300adba9037055728d2b48
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 14:49:49 +0530

    Add more

---
+++ PSOOP/Programs/complex_numbers.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+
+using namespace std;
+
+class complex 	//class definition of complex number
+{
+	float real;
+	float imaginary;
+	
+	public:		
+	complex()	//initializing a constructor named complex
+	{
+		real=0;
+		imaginary=0;
+	}
+	complex(float a, float b)	//parameterized constuctor
+	{
+		real=a;
+		imaginary=b;
+	}
+    
+	void putData(complex &number);
+	
+	complex friend operator + (complex a, complex b);	//prototyping a friend function
+	complex friend operator - (complex a, complex b);	//prototyping a friend function
+	
+	complex operator * (complex &number)		//function overloading
+	{
+		complex product;			//initializing object
+		product.real = (real*number.real) - (imaginary*number.imaginary);	//calculating multiplication
+		product.imaginary = (real*number.imaginary) + (number.real*imaginary);
+		return product;
+	}
+	
+	complex operator / (complex &number)		//function overloading
+	{
+		complex division;			//initializing object
+        float mod=(number.real*number.real + number.imaginary*number.imaginary);	//calculating division
+        division.real = ((real*number.real) + (imaginary*number.imaginary))/(mod);
+        division.imaginary = ((imaginary*number.real) - (real*number.imaginary))/(mod);
+		return division;
+	}
+};
+
+int main()
+{
+	complex answer;
+	int choice;
+	float Re, Im;
+	char repeat='y';	//initializing value of repeat to 'y', as initially it is y, the while loop will run once
+	
+	while(repeat=='y' || repeat=='Y')	//condition to check to continue program
+	{
+		cout << "First number\nEnter real part: ";
+		cin >> Re;					
+		cout << "Enter imaginary part: ";	
+		cin >> Im;	
+		complex number1(Re, Im);				//inputing the numbers
+		
+		cout << "\nSecond number\nEnter real part: ";
+		cin >> Re;
+		cout << "Enter imaginary part: ";
+		cin >> Im;
+		complex number2(Re, Im);
+		
+		cout << "\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nSelect the operation: ";	//menu
+		cin >> choice;
+		switch(choice)
+		{
+			case 1:
+				{
+					answer = number1 + number2;
+					answer.putData(answer);
+					break;
+				}
+			case 2:
+				{
+					answer = number1 - number2;
+					answer.putData(answer);
+					break;
+				}
+			case 3:
+				{
+					answer = number1 * number2;
+					answer.putData(answer);
+					break;
+				}
+			case 4:
+				{
+					answer = number1 / number2;
+					answer.putData(answer);
+					break;
+				}
+			default:
+				{
+					cout << "Invalid selection! Select a number between 1-4";
+					break;
+				}
+		}
+		cout << "\nDo you want to perform another operation? (y/n)";
+		cin >> repeat;
+	}
+	
+	return 0;
+}
+
+void complex::putData(complex &number)		//function to display the answer in correct format
+{
+	if(number.imaginary>=0)
+		cout << "\n" << real << "+" << imaginary << "i\n";
+	else
+		cout << "\n" << real << imaginary << "i\n";
+}
+
+complex operator + (complex a, complex b) 	//calculating addition
+{
+	complex sum;		//initializing object
+	sum.real = a.real + b.real;
+	sum.imaginary = a.imaginary + b.imaginary;
+	return sum;
+}
+
+complex operator - (complex a, complex b)	//calculating subtraction
+{
+	complex diff;		//initializing object
+	diff.real = a.real - b.real;
+	diff.imaginary = a.imaginary - b.imaginary;
+	return diff;
+}
---
+++ PSOOP/Programs/multiple_inheritance.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+
+class info
+{
+	char dob[11],name[50],bloodgroup[5];
+	public:
+	void getData();
+	void display();
+};
+void info :: getData()
+{
+	cout<<"\nEnter name: ";
+	cin.getline(name,50);
+	cin.getline(name,50);
+	cout<<"\nEnter date of birth (dd/mm/yyyy): ";
+	cin.getline(dob,11);
+	cout<<"\nEnter blood group: ";
+	cin.getline(bloodgroup,5);
+}
+void info :: display()
+{
+	cout<<"\n\nName: "<<name<<"\nDate of birth: "<<dob<<"\nBlood Group: "<<bloodgroup;
+}
+
+class physical
+{
+	int height,weight;
+	public:
+	void getDatap();
+	void displayp();
+};
+
+void physical :: getDatap()
+{
+	cout<<"\nEnter height: ";
+	cin>>height;
+	cout<<"\nEnter weight: ";
+	cin>>weight;
+}
+void physical :: displayp()
+{
+	cout<<"\nHeight: "<<height;
+	cout<<"\nWeight: "<<weight;
+}
+
+class insurance
+{
+	double policy_no;
+	char address[100];
+	public:
+	void getDatai();
+	void displayi();
+};
+
+void insurance :: getDatai()
+{
+	cout<<"\nEnter insurance policy number: ";
+	cin>>policy_no;
+	cout<<"\nEnter address: ";
+	cin.getline(address,100);
+	cin.getline(address,100);
+}
+
+void insurance :: displayi()
+{
+	cout<<"\nInsurance number: "<<policy_no;
+	cout<<"\nAddress: "<<address;
+}
+
+class data : public info, public physical, public insurance
+{
+	int phone_no;
+	char licence[20];
+	public:
+	void getDatad();
+	void displayd();
+};
+void data :: getDatad()
+{
+info:getData();
+physical:getDatap();
+insurance:getDatai();
+	  cout<<"\nEnter licence number: ";
+	  cin.getline(licence,20);
+	  cout<<"\nEnter phone number: ";
+	  cin>>phone_no;
+}
+void data :: displayd()
+{
+info:display();
+physical:displayp();
+insurance:displayi();
+	  cout<<"\nLicence number: "<<licence;
+	  cout<<"\nPhone number: "<<phone_no;
+}
+
+int main()
+{
+	int n,l;
+	cout<<"Enter number of data entries: ";
+	cin>>n;
+	data z[2];
+	for(l=1;l<=2;l++){
+	z[2].getDatad();}
+	for(l=1;l<=2;l++){
+	z[2].displayd();}
+	return 0;
+}
