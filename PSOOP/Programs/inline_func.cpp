804610f6c7e944b0cb14405b23bb4d06a43a9e1a
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 13:53:38 +0530

    Add inline_func.cpp

---
+++ PSOOP/Programs/inline_func.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+inline void data(int x)
+{
+	if(x%2==0)
+		cout<<x<<" is even\n";
+	else
+		cout<<x<<" is odd";
+}
+
+int main()
+{
+	data(10);
+	data(5);
+	return 0;
+}
+
