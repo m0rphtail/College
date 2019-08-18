3077fd21918ccab210b50965ddf16a7f95128d97
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-18 18:14:45 +0530

    update

--- PSOOP/Programs/copy_constructor.cpp
+++ PSOOP/Programs/copy_constructor.cpp
@@ -1,26 +1,30 @@
 //Copy Constuctor
 
-#include<iostream>
-using namespace std;
-class volume
-{
-	int s;
-	public:
-	volume(int & x)	//copy constuctor
-	{
-		s=x.s;	//copies value
-	}
-	void display()
-	{
-		cout<<"Volume is "<<s*s*s;
-	}
-};
 
-int main()
-{
-	volume a1;	//calls default constructor
-	volume a2(a1);	//calls copy constructor
-	a2.display();
-	return 0;
+#include<iostream> 
+using namespace std; 
+  
+class Point 
+{ 
+	private: 
+	int x, y; 
+	public: 
+	Point(int x1, int y1) { x = x1; y = y1; }//    Copy constructor 
+        Point(const Point &p2) {x = p2.x; y = p2.y; } 
+	           
+        int getX()            {  return x; } 
+        int getY()            {  return y; } 
+}; 
+		                     
+int main() 
+{ 
+Point p1(10, 15); // Normal constructor is called here 
+Point p2 = p1; // Copy constructor is called here                         
+             
+cout << "p1.x = " << p1.getX() << ", p1.y = " << p1.getY(); 
+cout << "\np2.x = " << p2.getX() << ", p2.y = " << p2.getY(); 
+	                                             
+return 0; 
 }
+			   
 	
