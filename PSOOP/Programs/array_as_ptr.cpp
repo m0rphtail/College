24783b856cc408638ef3241ea11e571cadb821d2
Kshitij Chitnis <47940274+KChitnis@users.noreply.github.com>
2019-08-19 13:44:40 +0530

    Update array_as_ptr.cpp

--- PSOOP/Programs/array_as_ptr.cpp
+++ PSOOP/Programs/array_as_ptr.cpp
@@ -1,21 +1,13 @@
 /* Write a program to display content of pointer using an array of pointers*/
 
-
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	const char* name[5];
-	int i;
-	name[0] = "a";
-	name[1] = "b";
-	name[2] = "c";
-	name[3] = "d";
-	name[4] = "e";
-	name[5] = "f";
-	for (i = 0; i <= 5; i++)
-	{
-		cout << *name[i];
-	}
-	return 0;
+  int arr[5] = { 1, 2, 3, 4, 5 };
+  int *ptr = arr;
+  cout<<"Address is "<<ptr;
+  return 0;
 }
+
