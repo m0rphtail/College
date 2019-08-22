ee5ce7151bfbce76c8191d94d65e2a436d865c4c
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-22 20:24:59 +0530

    Update

--- FDS/Assignment5-Sequential_File.c
+++ FDS/Assignment5-Sequential_File.c
@@ -7,8 +7,46 @@ v. Delete record */
 
 #include<stdio.h>
 
+struct student
+{
+	char name[50],grade;
+	int roll_no,marks;
+}s;
+
+void read();
+//void add();
+//void search();
+//void modify();
+//void delete();
+void display();
+
 int main()
 {
-
+	read();
+	display();
 	return 0;
 }
+
+void read()
+{
+	FILE *p;
+	p=fopen("Assignment5.txt", "w");
+	printf("Enter name: ");
+	scanf("%s", s.name);
+	fprintf(p,"%s",s.name);
+	fclose(p);
+}
+
+void display()
+{
+	FILE *q;
+	q=fopen("Assignment5.txt", "r");
+	do
+	{
+		fscanf(q,"%s",s.name);
+		printf("%s", s.name);
+	}
+	while(!feof(q));
+	fclose(q);
+}	
+
