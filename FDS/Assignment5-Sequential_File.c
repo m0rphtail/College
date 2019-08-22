747df541825ef272c5a9baf36999a74b5a9b3e58
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-22 19:13:39 +0530

    Add Assignment 5

---
+++ FDS/Assignment5-Sequential_File.c
@@ -0,0 +1,14 @@
+/*Implement sequential file and perform following operations: 
+i. Display 
+ii. Add records 
+iii. Search record 
+iv. Modify record 
+v. Delete record */
+
+#include<stdio.h>
+
+int main()
+{
+
+	return 0;
+}
