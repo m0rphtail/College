bb343754082578fd5ad0dd130acd442433dc5694
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:45:14 +0530

    clean

--- PSOOP/Programs/inline_func.cpp
+++ PSOOP/Programs/inline_func.cpp
@@ -1,18 +1,18 @@
-#include<iostream>     //KC
-using namespace std;     //KC
-     //KC
-inline void data(int x)     //KC
-{     //KC
-	if(x%2==0)     //KC
-		cout<<x<<" is even\n";     //KC
-	else     //KC
-		cout<<x<<" is odd";     //KC
-}     //KC
-     //KC
-int main()     //KC
-{     //KC
-	data(10);     //KC
-	data(5);     //KC
-	return 0;     //KC
-}     //KC
-//Kshitij Chitnis     //KC
+#include<iostream>
+using namespace std;
+
+inline void data(int x)
+{
+	if(x%2==0)
+		cout<<x<<" is even\n";
+	else
+		cout<<x<<" is odd";
+}
+
+int main()
+{
+	data(10);
+	data(5);
+	return 0;
+}
+//Kshitij Chitnis
