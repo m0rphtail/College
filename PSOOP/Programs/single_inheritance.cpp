cde7e9a093f32efb402c5897ad12e27b3f9907d9
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:48:29 +0530

    clean

--- PSOOP/Programs/single_inheritance.cpp
+++ PSOOP/Programs/single_inheritance.cpp
@@ -1,65 +1,65 @@
-#include<iostream>                                                //KC
-#include<string.h>                                                //KC
-                                                //KC
-using namespace std;                                                //KC
-                                                //KC
-class student                                                //KC
-{                                                //KC
-	int roll_no;                                                //KC
-	char name[50],branch[50];                                                //KC
-	public:                                                //KC
-	void getData();                                                //KC
-	void display();                                                //KC
-};                                                //KC
-                                                //KC
-class marks : public student                                                //KC
-{                                                //KC
-	int marks;                                                //KC
-	char grade[5];                                                //KC
-	public:                                                //KC
-	void getData1();                                                //KC
-	void display1();                                                //KC
-};                                                //KC
-                                                //KC
-                                                //KC
-void student :: getData()                                                //KC
-{                                                //KC
-//	cin.ignore();                                                //KC
-	cout<<"\nEnter roll no: ";                                                //KC
-	cin>>roll_no;                                                //KC
-	cout<<"\nEnter name: ";                                                //KC
-	cin.getline(name,50);                                                //KC
-	cout<<"\nEnter branch: ";                                                //KC
-	cin>>branch;                                                //KC
-}                                                //KC
-                                                //KC
-void student :: display()                                                //KC
-{                                                //KC
-	cout<<"\n\nRoll no: "<<roll_no;                                                //KC
-	cout<<"\nName: "<<name;                                                //KC
-	cout<<"\nBranch: "<<branch;                                                //KC
-}                                                //KC
-                                                //KC
-void marks :: getData1()                                                //KC
-{                                                //KC
-	student :: getData();                                                //KC
-	cout<<"\nEnter marks: ";                                                //KC
-	cin>>marks;                                                //KC
-	cout<<"\nEnter the grade: ";                                                //KC
-	cin>>grade;                                                //KC
-}                                                //KC
-                                                //KC
-void marks :: display1()                                                //KC
-{                                                //KC
-	student :: display();                                                //KC
-	cout<<"\nMarks: "<<marks;                                                //KC
-	cout<<"\nGrade: "<<grade;                                                //KC
-}                                                //KC
-                                                //KC
-int main()                                                //KC
-{                                                //KC
-	marks m;                                                //KC
-	m.getData1();                                                //KC
-	m.display1();                                                //KC
-	return 0;                                                //KC
-}//Kshitij Chitnis                                                //KC
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class student
+{
+	int roll_no;
+	char name[50],branch[50];
+	public:
+	void getData();
+	void display();
+};
+
+class marks : public student
+{
+	int marks;
+	char grade[5];
+	public:
+	void getData1();
+	void display1();
+};
+
+
+void student :: getData()
+{
+//	cin.ignore();
+	cout<<"\nEnter roll no: ";
+	cin>>roll_no;
+	cout<<"\nEnter name: ";
+	cin.getline(name,50);
+	cout<<"\nEnter branch: ";
+	cin>>branch;
+}
+
+void student :: display()
+{
+	cout<<"\n\nRoll no: "<<roll_no;
+	cout<<"\nName: "<<name;
+	cout<<"\nBranch: "<<branch;
+}
+
+void marks :: getData1()
+{
+	student :: getData();
+	cout<<"\nEnter marks: ";
+	cin>>marks;
+	cout<<"\nEnter the grade: ";
+	cin>>grade;
+}
+
+void marks :: display1()
+{
+	student :: display();
+	cout<<"\nMarks: "<<marks;
+	cout<<"\nGrade: "<<grade;
+}
+
+int main()
+{
+	marks m;
+	m.getData1();
+	m.display1();
+	return 0;
+}//Kshitij Chitnis
