e5d26fc7f523f03a04860ed47e208f1211741039
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:49:11 +0530

    clean

--- PSOOP/Programs/static_member_func.cpp
+++ PSOOP/Programs/static_member_func.cpp
@@ -1,34 +1,34 @@
-#include<iostream>                                                //KC
-using namespace std;                                                //KC
-                                                //KC
-class example                                                //KC
-{                                                //KC
-	int x;                                                //KC
-	static int y;                                                //KC
-	public:                                                //KC
-	void getdata()                                                //KC
-	{                                                //KC
-		cout<<"\nEnter value of x: ";                                                //KC
-		cin>>x;                                                //KC
-	}                                                //KC
-	void putdata()                                                //KC
-	{                                                //KC
-		cout<<"\nx is "<<x;                                                //KC
-	}                                                //KC
-	static void display()                                                //KC
-	{                                                //KC
-		y=y++;                                                //KC
-		cout<<"\ny is "<<y;                                                //KC
-	}                                                //KC
-};                                                //KC
-                                                //KC
-int example::y;                                                //KC
-                                                //KC
-int main()                                                //KC
-{                                                //KC
-	example a;                                                //KC
-	a.getdata();                                                //KC
-	a.putdata();                                                //KC
-	example::display();                                                //KC
-	return 0;                                                //KC
-}//Kshitij Chitnis                                                //KC
+#include<iostream>
+using namespace std;
+
+class example
+{
+	int x;
+	static int y;
+	public:
+	void getdata()
+	{
+		cout<<"\nEnter value of x: ";
+		cin>>x;
+	}
+	void putdata()
+	{
+		cout<<"\nx is "<<x;
+	}
+	static void display()
+	{
+		y=y++;
+		cout<<"\ny is "<<y;
+	}
+};
+
+int example::y;
+
+int main()
+{
+	example a;
+	a.getdata();
+	a.putdata();
+	example::display();
+	return 0;
+}//Kshitij Chitnis
