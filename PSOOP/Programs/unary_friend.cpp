8153a8d1c93b409543328e44cbb7ad9715814e69
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:50:24 +0530

    clean

--- PSOOP/Programs/unary_friend.cpp
+++ PSOOP/Programs/unary_friend.cpp
@@ -1,36 +1,36 @@
-#include<iostream>                                                //KC
-using namespace std;                                                //KC
-class UnaryFriend                                                //KC
-{                                                //KC
-     int a=10;                                                //KC
-     int b=20;                                                //KC
-     int c=30;                                                //KC
-     public:                                                //KC
-         void getvalues()                                                //KC
-         {                                                //KC
-              cout<<"Values of A, B & C\n";                                                //KC
-              cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<endl;                                                //KC
-         }                                                //KC
-         void show()                                                //KC
-         {                                                //KC
-              cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<endl;                                                //KC
-         }                                                //KC
-         void friend operator-(UnaryFriend &x);      //Pass by reference                                                //KC
-};                                                //KC
-void operator-(UnaryFriend &x)                                                //KC
-{                                                //KC
-     x.a = -x.a;     //Object name must be used as it is a friend function                                                //KC
-     x.b = -x.b;                                                //KC
-     x.c = -x.c;                                                //KC
-}                                                //KC
-int main()                                                //KC
-{                                                //KC
-     UnaryFriend x1;                                                //KC
-     x1.getvalues();                                                //KC
-     cout<<"Before Overloading\n";                                                //KC
-     x1.show();                                                //KC
-     cout<<"After Overloading \n";                                                //KC
-     -x1;                                                //KC
-      x1.show();                                                //KC
-      return 0;                                                //KC
-}//Kshitij Chitnis                                                //KC
+#include<iostream>
+using namespace std;
+class UnaryFriend
+{
+     int a=10;
+     int b=20;
+     int c=30;
+     public:
+         void getvalues()
+         {
+              cout<<"Values of A, B & C\n";
+              cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<endl;
+         }
+         void show()
+         {
+              cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<endl;
+         }
+         void friend operator-(UnaryFriend &x);      //Pass by reference
+};
+void operator-(UnaryFriend &x)
+{
+     x.a = -x.a;     //Object name must be used as it is a friend function
+     x.b = -x.b;
+     x.c = -x.c;
+}
+int main()
+{
+     UnaryFriend x1;
+     x1.getvalues();
+     cout<<"Before Overloading\n";
+     x1.show();
+     cout<<"After Overloading \n";
+     -x1;
+      x1.show();
+      return 0;
+}//Kshitij Chitnis
