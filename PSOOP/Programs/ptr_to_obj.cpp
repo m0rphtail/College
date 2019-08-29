96a9d987094586e9b2f3a3815c41a66939219ae2
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:48:08 +0530

    clean

--- PSOOP/Programs/ptr_to_obj.cpp
+++ PSOOP/Programs/ptr_to_obj.cpp
@@ -1,34 +1,34 @@
-#include<iostream>                                                //KC
-using namespace std;                                                //KC
-class store                                                //KC
-{                                                //KC
-private:                                                //KC
-	int code;                                                //KC
-	float price;                                                //KC
-public:                                                //KC
-	void accept();                                                //KC
-	void display();                                                //KC
-                                                //KC
-};                                                //KC
-void store::accept()                                                //KC
-{                                                //KC
-	cout << "\nEnter code & price: ";                                                //KC
-	cin >> code >> price;                                                //KC
-                                                //KC
-}                                                //KC
-void store::display()                                                //KC
-{                                                //KC
-	cout << "\nCode & Price is: " << code << price;                                                //KC
-}                                                //KC
-int main()                                                //KC
-{                                                //KC
-	int i;                                                //KC
-	store* s = new store[3];                                                //KC
-	for (i = 0; i < 3; i++)                                                //KC
-	{                                                //KC
-		s->accept();                                                //KC
-		s->display();                                                //KC
-		s++;                                                //KC
-	}                                                //KC
-	return 0;                                                //KC
-}//Kshitij Chitnis                                                //KC
+#include<iostream>
+using namespace std;
+class store
+{
+private:
+	int code;
+	float price;
+public:
+	void accept();
+	void display();
+
+};
+void store::accept()
+{
+	cout << "\nEnter code & price: ";
+	cin >> code >> price;
+
+}
+void store::display()
+{
+	cout << "\nCode & Price is: " << code << price;
+}
+int main()
+{
+	int i;
+	store* s = new store[3];
+	for (i = 0; i < 3; i++)
+	{
+		s->accept();
+		s->display();
+		s++;
+	}
+	return 0;
+}//Kshitij Chitni
