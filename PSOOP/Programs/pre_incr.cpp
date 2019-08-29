699c9d081ba2979f25d88d86a5b9324daa216117
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-08-29 19:47:44 +0530

    clean

--- PSOOP/Programs/pre_incr.cpp
+++ PSOOP/Programs/pre_incr.cpp
@@ -1,40 +1,40 @@
-#include<iostream>                                                //KC
-using namespace std;                                                //KC
-class incr                                                //KC
-{                                                //KC
-	int x;                                                //KC
-	public:                                                //KC
-		incr()                                                //KC
-		{                                                //KC
-			x=1;                                                //KC
-		}                                                //KC
-		void display();                                                //KC
-		void operator++();                                                //KC
-		void operator++(int);                                                //KC
-		                                                //KC
-};                                                //KC
-void incr::display()                                                //KC
-{                                                //KC
-	cout<<x<<endl;                                                //KC
-		                                                //KC
-}                                                //KC
-void incr::operator++()                                                //KC
-{                                                //KC
-	++x;                                                //KC
-}                                                //KC
-void incr::operator++(int)                                                //KC
-{                                                //KC
-	x++;                                                //KC
-}                                                //KC
-int main()                                                //KC
-{                                                //KC
-	incr i1;                                                //KC
-	i1.display();                                                //KC
-	++i1;                                                //KC
-	i1.display();                                                //KC
-	i1++;                                                //KC
-	i1.display();                                                //KC
-	return 0;                                                //KC
-}                                                //KC
-                                                //KC
-//Kshitij Chitnis                                                //KC
+#include<iostream>
+using namespace std;
+class incr
+{
+	int x;
+	public:
+		incr()
+		{
+			x=1;
+		}
+		void display();
+		void operator++();
+		void operator++(int);
+		
+};
+void incr::display()
+{
+	cout<<x<<endl;
+		
+}
+void incr::operator++()
+{
+	++x;
+}
+void incr::operator++(int)
+{
+	x++;
+}
+int main()
+{
+	incr i1;
+	i1.display();
+	++i1;
+	i1.display();
+	i1++;
+	i1.display();
+	return 0;
+}
+
+//Kshitij Chitni
