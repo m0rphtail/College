34dda992574078ffcf0db7cfd95d53f2a817e5e2
Kshitij Chitnis <chitniskshitij@gmail.com>
2019-09-17 11:54:34 +0530

    update Function Template

--- PSOOP/Assignments/Assignment7-Function_Template.cpp
+++ PSOOP/Assignments/Assignment7-Function_Template.cpp
@@ -41,26 +41,25 @@ void sub(M a[][size],S b[][size])
 template <class M,class S>
 void mul(M a[][size],S b[][size])
 {
- S c[size][size];
- for(int i=0;i<size;i++)
-    {
-    for(int j=0;j<size;j++)
-    {
-      for(int k=0;k<size;k++)
-        c[i][j]=a[i][k]*b[k][j];
-     }
-    }
- for(int i=0;i<size;i++)
-      {
-    for(int j=0;j<size;j++)
-    {
-     cout<<"   "<<c[i][j];
-      }
-    cout<<endl;
-    }
+	S c[size][size];
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			for(int k=0;k<size;k++)
+			c[i][j]=a[i][k]*b[k][j];
+		}
+	}
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			cout<<"   "<<c[i][j];
+		}
+		cout<<endl;
+	}
 }
 
-//transpose
 template <class M>
 void trans(M a[][size])
 {
